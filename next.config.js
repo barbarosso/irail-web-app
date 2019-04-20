8dba499da379e56051fbffa396a350cc1654abbb
barbarosso <vanhecke.pascal@gmail.com>
2019-04-20 15:07:14 +0200

    readded serverles to next config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const withSass = require("@zeit/next-sass");
 module.exports = withSass({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: "empty"
-    };
+    target: "serverless",
+      (config.node = {
+        fs: "empty"
+      });
 
     return config;
   }
